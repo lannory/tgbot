6c7a64ddf501919a0ada4e6ec1c85763aabb6a77
lannory <detochkanikolay@gmail.com>
2025-10-28 18:00:33 +0200

    added settings for pm2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,17 +1,25 @@
 export default {
   apps: [
     {
-      name: 'backend',
-      script: './server.js',
-      cwd: '/root/tgbot/server',
+      name: "backend",
+      script: "./server.js",
+      cwd: "/root/tgbot/server",
+      interpreter: "node",
       watch: false,
-      instances: 1,
-      autorestart: true,
-      max_restarts: 10,
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
         PORT: 3000
+      }
+    },
+    {
+      name: "bot",
+      script: "./script.js",
+      cwd: "/root/tgbot/client",
+      interpreter: "node",
+      watch: false,
+      env: {
+        NODE_ENV: "production"
       }
     }
   ]
-};
+}
