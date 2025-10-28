4a80e876afac802330d219edd03c4aa167fc62c6
lannory <detochkanikolay@gmail.com>
2025-10-28 23:29:10 +0200

    TRYING TO FIX STH

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
       script: "server.js",
       cwd: "/root/tgbot/server",
       interpreter: "node",
+      node_args: "--experimental-specifier-resolution=node",
       watch: false,
       env: {
         NODE_ENV: "production",
