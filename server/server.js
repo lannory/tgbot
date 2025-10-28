dc4f1851b2855fa7a8f576dbdbf90d31bf4c2abf
lannory <detochkanikolay@gmail.com>
2025-10-28 15:29:19 +0200

    some fixes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,13 @@ import fs from 'fs';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
 dotenv.config();
